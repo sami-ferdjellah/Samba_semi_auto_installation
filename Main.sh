2b97ae8cd908e53d3124e8ace0fbdfd0b6f4361d
Sami_F <sami.ferdjellah@live.fr>
2025-01-12 15:52:54 +0100

    Update Main.sh
    ajout de comentaires et mise en page

--- Main.sh
+++ Main.sh
@@ -1,4 +1,5 @@
 # Samba_semi_auto_installation
+
 # based on this guide
 # https://ubuntu.com/tutorials/install-and-configure-samba#1-overview
 
@@ -13,16 +14,20 @@ usr/share/man/man8/samba.8.gz
 #create a directory
 mkdir /home/<username>/sambashare/
 
+# modification du fichier de configuration
 nano /etc/samba/smb.conf
 
-[sambashare]
-    comment = Samba on Ubuntu
-    path = /home/username/sambashare
-    read only = no
-    browsable = yes
+# [sambashare]
+#   comment = Samba on Ubuntu
+#    path = /home/username/sambashare
+#    read only = no
+#    browsable = yes
 
+# redemaragae de samba 
 sudo service smbd restart
 
+# modification du firewall
 sudo ufw allow samba
 
+# creation du pwd
 sudo smbpasswd -a username
